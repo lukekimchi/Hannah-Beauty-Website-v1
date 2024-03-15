6a1457bec2c3763ab76b20c5766e0c9caad74d7d
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2024-03-16 10:57:32 +1300

    Fix gallery image src pathnames

--- src/constants/gallery.js
+++ src/constants/gallery.js
@@ -1,18 +1,18 @@
 export const galleryImages = [
   {
     id: 1,
-    src: "./assets/brows/brows_7_after copy.webp",
+    src: "src/assets/brows/brows_7_after copy.webp",
   },
   {
     id: 2,
-    src: "./assets/lash_lift/lash_lift_3.webp",
+    src: "src/assets/lash_lift/lash_lift_3.webp",
   },
   {
     id: 3,
-    src: "./assets/eyeliner/eyeliner_1_after copy.webp",
+    src: "src/assets/eyeliner/eyeliner_1_after copy.webp",
   },
   {
     id: 4,
-    src: "./assets/lip_blush/lip_2_after copy.webp",
+    src: "src/assets/lip_blush/lip_2_after copy.webp",
   },
 ];
