070ca46b480d7c59061a42facaeeb33aee6942f0
Luke Kim <lukekim117@gmail.com>
2025-08-09 02:23:07 +1000

    Remove HBA Coming Soon message from Blog page

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import MainLayout from "../layout/MainLayout";
 import { motion as m } from "framer-motion";
-import { fadeInDown, fadeInUp, fadeInLeft, fadeInRight } from "../constants/animations";
+import { fadeInDown, fadeInUp } from "../constants/animations";
 
 const BlogPage = () => {
   return (
     <MainLayout>
       <div className="flex flex-col h-[2000px]">
-        <m.div className="flex flex-col items-center mt-20" >
+        {/* <m.div className="flex flex-col items-center mt-20" >
           <m.img
             className="h-[100px] object-cover mt-5 mb-2"
             src="./images/logo.webp"
@@ -15,7 +14,7 @@ const BlogPage = () => {
             {...fadeInLeft}
           />
           <m.p className="text-lg text-black text-center" {...fadeInRight}>Hannah Beauty<br/>Academy</m.p>
-        </m.div>
+        </m.div> */}
         <div className="h-fit text-center mt-10 p-10">
           <m.h3 {...fadeInDown(0)}>COMING SOON</m.h3>
           <m.p {...fadeInUp(0.2)}>Stay tuned.</m.p>
